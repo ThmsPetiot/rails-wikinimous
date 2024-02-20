70dcb3c2a1b6fd1291ce22cba9bd8ef26e72c46d
thomas petiot <thomas.petiot.01@gmail.com>
2024-02-20 15:29:14 +0100

    ex3

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+10.times do
+  new_article = Article.new(title: Faker::Vehicle.manufacture, content: Faker::Vehicle.model)
+  new_article.save
+end
